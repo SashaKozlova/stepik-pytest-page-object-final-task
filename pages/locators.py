4a0ee6883d5964b9b7284cf908f476af1868ae2b
Aleksandra Kozlova <a.kozlova@naveksoft.com>
2021-06-23 18:40:24 +0300

    empty file __init__ for relative imports to work

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_ADDED = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    PRICE_BOOK = (By.CSS_SELECTOR, ".product_main > p.price_color")
+    PRICE_BASKET = (By.CSS_SELECTOR, "div.alertinner > p:nth-child(1) > strong")
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def should_be_promo_url(self):
+        assert "?promo=NewYear" in self.browser.current_url, "URL does not contain ?promo="
+
+    def add_to_basket(self):
+        button_add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        button_add_to_basket.click()
+
+    def should_be_added_in_basket(self):
+        message_product_added = self.browser.find_element(*ProductPageLocators.BOOK_ADDED).text
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert product_name == message_product_added,\
+            "Name of the book: {product_name} does not mach with {message_product_added}."
+
+    def should_be_basket_price(self):
+        basket_price = self.browser.find_element(*ProductPageLocators.PRICE_BASKET).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE_BOOK).text
+        print('check basket')
+        assert product_price == basket_price, "Product price {product_price} not equal {basket_price}."
